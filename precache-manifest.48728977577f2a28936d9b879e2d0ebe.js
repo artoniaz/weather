eb0c8033d86a1c9c52a36d3169e0479a4e4296a2
artoniaz <artoni.az@gmail.com>
2019-03-15 12:27:04 +0100

    Updates

---
+++ precache-manifest.48728977577f2a28936d9b879e2d0ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c53d949fc595b6264a8546b22a92cb20",
+    "url": "/weather/static/media/kielce.c53d949f.jpg"
+  },
+  {
+    "revision": "3cb3cde5c978189545b4",
+    "url": "/weather/static/js/runtime~main.6cff1e35.js"
+  },
+  {
+    "revision": "94831de3729e451a6932",
+    "url": "/weather/static/js/main.2905807d.chunk.js"
+  },
+  {
+    "revision": "36701f583bc46cfcb9fd",
+    "url": "/weather/static/js/2.cff1f837.chunk.js"
+  },
+  {
+    "revision": "94831de3729e451a6932",
+    "url": "/weather/static/css/main.9e6a4bbd.chunk.css"
+  },
+  {
+    "revision": "2123a10a314601ebb3829d75e3d3dff6",
+    "url": "/weather/index.html"
+  }
+];
